96f61646b0cfe72189405cfe64d34a6c82d9aa17
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-30 16:38:16 +0700

    context

--- src/App.js
+++ src/App.js
@@ -2,38 +2,87 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//================ Composition Children Props
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome/>
+//       <p>Contents</p>
+//       <Warning/>
+//     </div>
+//   );
+// }
+//
+// function Welcome(){
+//   return (
+//     <Notification status="success">
+//       <h1>Welcome</h1>
+//       <p>Congratulations</p>
+//     </Notification>
+//   )
+// }
+//
+// function Warning(){
+//   return (
+//     <Notification status="warning">
+//       <p>Don't forget to pay your Invoice</p>
+//     </Notification>
+//   )
+// }
+//
+// function Notification(props){
+//   return (
+//     // <div className="notification">
+//     <div className={'notification is-'+props.status}>
+//       {props.children}
+//     </div>
+//   )
+// }
+
+
+// ============= Context
+
+const MyContext = React.createContext();
+
 function App() {
+
   return (
-    <div>
-      <Welcome/>
-      <p>Contents</p>
-      <Warning/>
-    </div>
+    <MyContext.Provider value="blue">
+      <header>
+        <Navigation color="red"/>
+      </header>
+    </MyContext.Provider>
   );
 }
 
-function Welcome(){
+function Navigation(props){
   return (
-    <Notification status="success">
-      <h1>Welcome</h1>
-      <p>Congratulations</p>
-    </Notification>
+    <div>
+      <a href='#'>Home</a>
+      <a href='#'>Contact</a>
+
+      <Search color={props.color}/>
+      <Button />
+    </div>
   )
 }
 
-function Warning(){
+function Button(){
   return (
-    <Notification status="warning">
-      <p>Don't forget to pay your Invoice</p>
-    </Notification>
+    <MyContext.Consumer>
+      {(context) => (
+        <button>Color we get is {context}</button>
+      )}
+    </MyContext.Consumer>
   )
 }
 
-function Notification(props){
+function Search(props){
   return (
-    // <div className="notification">
-    <div className={'notification is-'+props.status}>
-      {props.children}
+    <div>
+      <label>Search</label>
+      <input type="search" placeholder={props.color} />
     </div>
   )
 }
